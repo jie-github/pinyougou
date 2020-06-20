3a37b1931dcce65de60e2a56dc6e9b4302348a75
jie-github <164352291@qq.com>
2020-06-20 19:25:17 +0800

    delete

--- src/main/api/main.java
+++ src/main/api/main.java
@@ -1,13 +1,5 @@
 public class main{
     public static void main(String[] args) {
         System.out.println("你好");
-        System.out.println("在mster分支做修改");
-        System.out.println("在mster分支做修改");
-        System.out.println("在dev4分支做修改");
-        System.out.println("在dev4分支做修改");
-        System.out.println("在dev4分支做修改");
-        System.out.println("在dev4分支做修改");
-        System.out.println("在dev4分支做修改");
-        System.out.println("在dev4分支做修改");
     }
 }
